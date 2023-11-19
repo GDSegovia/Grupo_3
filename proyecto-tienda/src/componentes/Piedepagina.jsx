5e5bdddbcc34ef2be4dbe90f1e303ba44d0639ff
David Segovia <segovia001@gmail.com>
2023-11-19 18:34:46 -0300

    Aregle Piedepagina.jsx

--- proyecto-tienda/src/componentes/Piedepagina.jsx
+++ proyecto-tienda/src/componentes/Piedepagina.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Piedepagina';
+import './Piedepagina.css';
 
 function Piedepagina() {
     return (
@@ -17,3 +17,4 @@ function Piedepagina() {
     )
 }
  
+export default Piedepagina;
