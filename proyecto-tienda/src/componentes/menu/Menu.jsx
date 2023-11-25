e5e176d0e8575a0e75002405c08bf6f46caeb3c1
David Amhed Beltrán Rubín de Celis <davikin_01@hotmail.com>
2023-11-25 20:12:49 -0300

    Instalando reac-router-dom

---
+++ proyecto-tienda/src/componentes/menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        <link rel="stylesheet" href="" />
+        <link rel="stylesheet" href="" />
+        <link rel="stylesheet" href="" />
+        <link rel="stylesheet" href="" />
+      </ul>
+    </nav>
+  );
+}
