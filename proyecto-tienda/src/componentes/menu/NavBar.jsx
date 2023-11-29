0a42eb2689dfa85abf76c42b83f095619d4768d4
David Amhed Beltrán Rubín de Celis <davikin_01@hotmail.com>
2023-11-29 08:45:08 -0300

    Actualizando navBar e informando que se necesita la dependencia de: $ npm i styled-components

--- proyecto-tienda/src/componentes/menu/NavBar.jsx
+++ proyecto-tienda/src/componentes/menu/NavBar.jsx
@@ -2,7 +2,7 @@
 // import { Router, Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-// import BurgerButton from '../../Pruebas/BurgerButton';
+import BurgerButton from './BurgerButton';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import Carro from '../../assets/icons/Icon-shopping-cart.svg'
@@ -35,7 +35,7 @@ function NavBar() {
       <a className='MiniCars' href='/'>MiniCars Toys</a>
       <BgDiv className={`links ${clicked ? 'active' : ''}`}></BgDiv>
       <div className='burger'>
-        {/* <BurgerButton clicked={clicked} handleClick={handleClick} /> */}
+        <BurgerButton clicked={clicked} handleClick={handleClick} />
       </div>
       <nav className={`links ${clicked ? 'active' : ''}`}>
         <Link to="/Reseñas">Reseñas</Link>
